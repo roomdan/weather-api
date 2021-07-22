27abc5e803b16216341b0b284524be7189262217
daniel <danielfcramos7@gmail.com>
2021-07-21 21:58:00 -0500

    change api url

--- src/apiCallweather.js
+++ src/apiCallweather.js
@@ -1,8 +1,10 @@
 export const WatherApi = async (long)=>{
-   let url = `https://api.weatherapi.com/v1/current.json?key=d772c31a2ddb48fcbed220743210507&q=${long}&aqi=no` ;
+   let url = `http://api.weatherapi.com/v1/current.json?key=d772c31a2ddb48fcbed220743210507EN&q=${long}&aqi=no` ;
    const fetchUrl = await fetch(url); 
    const data = await fetchUrl.json();
    return data
 }
+
+
 
 // `http://api.weatherapi.com/v1/current.json?key=d30ef1887425411aa27220443210507&q=${long}&aqi=no`
