16c4f136c49916634dc46cac1ae1e27daf1cbe29
daniel <danielfcramos7@gmail.com>
2021-07-23 20:26:53 -0500

    change http protocole for https

--- src/apiCallweather.js
+++ src/apiCallweather.js
@@ -1,5 +1,5 @@
 export const WatherApi = async (long)=>{
-   let url = `http://api.weatherapi.com/v1/current.json?key=d772c31a2ddb48fcbed220743210507&q=${long}&aqi=no`;
+   let url = `https://api.weatherapi.com/v1/current.json?key=d772c31a2ddb48fcbed220743210507&q=${long}&aqi=no`;
    const fetchUrl = await fetch(url); 
    const data = await fetchUrl.json();
    return data
